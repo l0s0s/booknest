2d397099dd3afb486613fd88c2aa35a24bd0460a
Pavel Ogurtsov <cucumb3rpasha@gmail.com>
2024-04-19 16:25:52 +0300

    change favicon and title

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -6,11 +6,11 @@ function ColorSchemesExample() {
   return (
     <Navbar bg="dark" data-bs-theme="dark">
     <Container>
-        <Navbar.Brand href="#home">Booknest</Navbar.Brand>
+        <Navbar.Brand href="home">Booknest</Navbar.Brand>
         <Nav className="me-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#features">Book list</Nav.Link>
-        <Nav.Link href="#pricing">Brows</Nav.Link>
+        <Nav.Link href="home">Home</Nav.Link>
+        <Nav.Link href="list">Book list</Nav.Link>
+        <Nav.Link href="browse">Browse</Nav.Link>
         </Nav>
     </Container>
     </Navbar>
