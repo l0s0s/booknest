ddef4f857c5ff9d8f54e080185461b3dbb23f9ac
Pavel Ogurtsov <cucumb3rpasha@gmail.com>
2024-04-22 18:54:57 +0300

    implement browse - top 10 books

--- src/components/Browse.tsx
+++ src/components/Browse.tsx
@@ -1,8 +1,29 @@
+import books from '../data/top_books/top_books.json';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+
+
 const Browse = () => {
     return (
-        <div>
-            <h1>WIP</h1>
-        </div>
+        <Container>
+            <Row>
+                {books.map((book, index) => (
+                    <Col key={index} md={2}>
+                        <br />
+                        <Card className="d-flex flex-column" style={{ cursor: 'pointer' }}>
+                            <span title={book.title}>
+                                <Card.Img className="img-fluid" variant="top" src={book.image} style={{ aspectRatio: '469/727' }} />
+                                <Card.Body className="flex-grow-1">
+                                    <Card.Title className="text-truncate" style={{ fontSize: "15px" }}>{book.title}</Card.Title>
+                                </Card.Body>
+                            </span>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+        </Container>
     );
 };
 
